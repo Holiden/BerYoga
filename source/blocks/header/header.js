68ed34cb6181ba2478ab580ac1c19281716cf35c
Alexander Holiden Diakonov <holideny@gmail.com>
2020-05-02 16:28:50 +0700

    Убирает ограничение прокрутки страницы

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,9 +1,7 @@
-const body = document.querySelector('.main-page');
 const menu = document.querySelector('.navigation');
 const button = document.querySelector('.menu');
 
 button.addEventListener('click', () => {
-  body.classList.toggle('main-page--active');
   menu.classList.toggle('navigation--active');
   button.classList.toggle('menu--active');
 });
