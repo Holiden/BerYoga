aefc30c76b6b25d93450123473dbf0e1314b9bbd
Alexander Holiden Diakonov <holideny@gmail.com>
2020-05-09 22:07:49 +0700

    Рефакторит меню

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,7 +1,31 @@
-const menu = document.querySelector('.navigation');
-const button = document.querySelector('.menu');
+(function () {
+  const navigationMenuButton = document.querySelector('.menu');
+  const navigationMenu = document.querySelector('.navigation');
 
-button.addEventListener('click', () => {
-  menu.classList.toggle('navigation--active');
-  button.classList.toggle('menu--active');
-});
+  const isMenuHiddenCheck = () => {
+    let attributeValueExpanded = navigationMenuButton.getAttribute('aria-expanded');
+
+    return attributeValueExpanded === 'true';
+  };
+
+  const handleNavigationMenuState = () => {
+    const isMenuHidden = isMenuHiddenCheck();
+    navigationMenuButton.setAttribute('aria-expanded', !isMenuHidden);
+
+    if (isMenuHidden) {
+      navigationMenuButton.setAttribute('aria-label', 'Открыть меню');
+    } else {
+      navigationMenuButton.setAttribute('aria-label', 'Закрыть меню');
+    }
+  };
+
+  const toggleNavigationMenu = () => {
+    navigationMenuButton.classList.toggle('menu--active');
+    navigationMenu.classList.toggle('navigation--active');
+  };
+
+  navigationMenuButton.addEventListener('click', () => {
+    handleNavigationMenuState();
+    toggleNavigationMenu();
+  });
+}());
